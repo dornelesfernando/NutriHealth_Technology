1f95ee203951c875839a0dfbf0653398fedac953
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-12-12 10:06:40 -0300

    header component

---
+++ header.php
@@ -0,0 +1,17 @@
+<!-- Barra de navegação -->
+<header>
+    <div class="logo">
+        NutriHealth Technology
+        <i class="fa fa-person-cane"></i>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="ppi01.php" class="active"><i class="fa fa-info-circle"></i> Sobre</a></li>
+            <li><a href="objetivos.php"><i class="fa fa-bullseye"></i> Objetivos</a></li>
+            <li><a href="exercicios.php"><i class="fa fa-dumbbell"></i> Exercícios</a></li>
+            <li><a href="dicas.php"><i class="fa fa-heart"></i> Dicas</a></li>
+            <li><a href="materiais.php"><i class="fa fa-book"></i> Materiais</a></li>
+            <li><a href="perfil.php"><i class="fa fa-book"></i> Perfil</a></li>
+        </ul>
+    </nav>
+</header>
