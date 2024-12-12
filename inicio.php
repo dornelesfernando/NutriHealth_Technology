22e51d0066e7554ae3544d83579996fcb33b3af9
Fernando Dorneles <fernandodorneles95@gmail.com>
2024-12-12 10:06:11 -0300

    initial page

---
+++ inicio.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página Inicial</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="./src/css/styleInicio.css">
+</head>
+<body>
+
+    <div class="container">
+        <h1>Bem-vindo ao NutriHealth Technology</h1>
+        <p>Aqui você pode acessar suas informações de saúde e muito mais. Faça login ou crie uma conta para começar.</p>
+
+        <div class="box">
+            <a href="login.php">Login</a>
+            <a href="cadastro.php">Cadastro</a>
+        </div>
+    </div>
+
+</body>
+</html>
